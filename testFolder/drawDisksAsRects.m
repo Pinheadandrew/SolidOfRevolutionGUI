de4ca40b868464f006f30cccf535a14ef750ce3e
Andrew <siggles1418@gmail.com>
2018-03-22 10:10:16 -0400

    Finished drawDisksAsRects test function so that it works with both axis directions (x and y). Use code in this function as basis of code that draws rectangles in VUC's plot.

--- testFolder/drawDisksAsRects.m
+++ testFolder/drawDisksAsRects.m
@@ -1,37 +1,35 @@
 function drawDisksAsRects(funcString, lowbound, upbound, subdivs, axisOri, axisVal)
-%DRAWDISKSASRECTS Summary of this function goes here
-%   Detailed explanation goes here
 % THIS WORKS IN REFLECTING AROUND AXIS PARALLEL TO X-AXIS
 % Test file that draws rectangles representing disks viewed from the side, 
 % their heights are the diameters of the disks. Flexible for any arbitrary 
 % value as y-axis to rotate area around. Only works with 
-
-% Supposed user inputs
-axisOri = "y";
-
+clf
 syms x
 f(x) = str2sym(funcString);
 
 steps = (upbound - lowbound)/subdivs;
 midpoints = lowbound+(steps/2):steps:upbound-(steps/2);
-xthings = lowbound:steps:upbound;
+rectAxisPoints = lowbound:steps:upbound;
 
-% Get heights of rectangles b/w axis and function line. Will use as disk
-% radii, stretch the diameter to make area reflect across axis to simulate
-% perpendicular rotation.
-diskRadii = abs(double(f(midpoints) - axisVal));
+%FOR TESTING FUNCTION BY ITSELF:
+if(axisOri =="y")
+    diskRadii = abs(double(f(midpoints) - axisVal));
 
-axisLine = fplot(axisVal), hold on;
-line = fplot(f(x)), hold on;
-xlim([lowbound upbound]);
-
-xverts = [xthings(1:end-1); xthings(1:end-1); xthings(2:end); xthings(2:end)];
-% Use yverts to derive of way to get full height.
-yverts = [axisVal-diskRadii; axisVal + diskRadii;...
-            axisVal + diskRadii; axisVal - diskRadii];
-
+    xverts = [rectAxisPoints(1:end-1); rectAxisPoints(1:end-1);... 
+        rectAxisPoints(2:end); rectAxisPoints(2:end)];
+    yverts = [axisVal-diskRadii; axisVal + diskRadii;...
+                axisVal + diskRadii; axisVal - diskRadii];
+%     fplot(axisVal, [lowbound upbound], "g");
+    fplot(f(x), [lowbound, upbound], "r"), hold on;
+else
+    g(x) = finverse(f(x));
+    diskRadii = abs(double(g(midpoints)-axisVal));
+    fplot(f(x), [double(g(lowbound)), double(g(upbound))], "r"), hold on;
+    xverts = [axisVal-diskRadii; axisVal-diskRadii;...
+                axisVal + diskRadii; axisVal + diskRadii];
+    yverts = [rectAxisPoints(1:end-1); rectAxisPoints(2:end);...
+                          rectAxisPoints(2:end); rectAxisPoints(1:end-1)];
+end
 patch(xverts, yverts, "b");
-uistack(line, "top");
-uistack(axisLine, "top");
 end
 
