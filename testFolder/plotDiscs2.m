f02a1b5e74f8fd702f01201c6614b80b2f21a175
Andrew <siggles1418@gmail.com>
2018-04-08 08:41:47 -0400

    Added temporary function for plotting discs rotated around y-axis.
    Also getting started on plotting axis-line that discs revolve around in plotDiscs.

--- testFolder/plotDiscs2.m
+++ testFolder/plotDiscs2.m
@@ -1,10 +1,10 @@
-function plotDiscs(funcString, lowbound, upbound, cylsCount, y_axis_val)
-%Testing drawing of solids of revolution using disc method, with area under function
-% f(x) = x rotated around y=0. Input bounds of solid, along with number of
+function plotDiscs2(funcString, lowbound, upbound, cylsCount, y_axis)
+%Testing drawing of solids of revolution using disc method, with area under/above specified 
+% symbolic expression of x rotated around x=0. Input bounds of solid, along with number of
 % discs to approximate the volume.
     clf
     syms x
-    f(x) = str2sym(funcString);
+    f(x) = finverse(str2sym(funcString));
     theta=0:pi/30:2*pi;
     
     diskWidth = (upbound-lowbound)/cylsCount; %<- Thickness of each disk
@@ -19,39 +19,33 @@ function plotDiscs(funcString, lowbound, upbound, cylsCount, y_axis_val)
       [x, y, z] = cylinder(diskRadii(i), length(theta)-1); %The function at x (in this loop, i) is the radius of a cylinder
       cyl = surf(x, y, z); 
       hold on
-      rotate(cyl, [0 1 0], 90)
 
-      cyl.XData(1, :) = cylMargins(i);
-      cyl.XData(2, :) = cylMargins(i+1);
+      cyl.ZData(1, :) = cylMargins(i);
+      cyl.ZData(2, :) = cylMargins(i+1);
       
-      y_distance = diskRadii(i)*cos(theta);
-      z_distance = diskRadii(i)*sin(theta);
+      x_distance = diskRadii(i)*cos(theta);
+      y_distance = diskRadii(i)*sin(theta);
       
       % Setting Z and Y coords so the points of cylinder's edge are equal
       % distance from the axis of rotation.
 
        cyl.YData(1, :) = y_distance;
        cyl.YData(2, :) = y_distance;
-       cyl.ZData(1, :) = z_distance;
-       cyl.ZData(2, :) = z_distance;
+       cyl.XData(1, :) = x_distance;
+       cyl.XData(2, :) = x_distance;
        
-       cir_x_1 = cylMargins(i)+zeros(size(theta));
-       cir_x_2 = cylMargins(i+1)+zeros(size(theta));
+       cir_z_1 = cylMargins(i)+zeros(size(theta));
+       cir_z_2 = cylMargins(i+1)+zeros(size(theta));
+       cir_x = x_distance;
        cir_y = y_distance;
-       cir_z = z_distance;
        
        %Draws both front and back faces of each disc.
-       patch(cir_x_1,cir_y,cir_z,	[0.4660, 0.6740, 0.1880])
-       patch(cir_x_2,cir_y,cir_z,	[0.4660, 0.6740, 0.1880])
+       patch(cir_x,cir_y,cir_z_1,	[0.4660, 0.6740, 0.1880])
+       patch(cir_x,cir_y,cir_z_2,	[0.4660, 0.6740, 0.1880])
        set(cyl,'edgecolor','none')
     end
     
-    plot3(midpoints, zeros(1, length(midpoints)), diskRadii, "LineWidth", 2, "Color", "r") 
-    % Draws axis of rotation
-    axisLims = [cylMargins(1)-1 cylMargins(end)+1];
-    plot3(axisLims, zeros(1, length(axisLims)), zeros(1, length(axisLims)),...
-      "LineWidth", 2, "Color", "b")
-    xlim(axisLims);
+    %plot3(midpoints, zeros(1, length(midpoints)), diskRadii, "LineWidth", 2, "Color", "r") 
     xlabel('X')
     ylabel('Y')
     zlabel('Z')
