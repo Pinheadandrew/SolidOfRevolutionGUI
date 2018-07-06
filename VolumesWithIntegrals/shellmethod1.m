1d8ede39c20801899018776c1f6fabbb1acf7870
Andrew <siggles1418@gmail.com>
2018-07-05 22:38:18 -0400

    Finishing touches to make same solid out of shells and discs
    Automatically updates bounds of volume when switching b/w disk and shell methods in order to have same volume between both versions of solid.
    
    Filler cylinders for both 3D and 2D representations.
    
    Axis of rotation reverts to what it was before user selects different axis orientation, hits Cancel in input dialog.

--- VolumesWithIntegrals/shellmethod1.m
+++ VolumesWithIntegrals/shellmethod1.m
@@ -17,16 +17,45 @@ function volume = shellmethod1(funcString, lowerBound, upperBound, axisOri, axis
 syms x
 assume(x >= lowerBound & x <= upperBound);
 f(x) = str2sym(funcString);
+g(x) = finverse(f);
 radius = x - axisValue;
 
+% If rotating around vertical line, 
 if (lower(axisOri) == 'x')
-    g(x) = finverse(f);
     integrant = abs(radius*(double(f(upperBound))-f(x)));
     volume = 2*pi*double(int(integrant, lowerBound, upperBound));
 else % If rotation done paralell to a vertical line, integrate function's inverse.
-    A(x) = finverse(f(x));
-    integrant = abs(radius*(A(upperBound) - A(x)));
+    integrant = abs(radius*(g(upperBound) - g(x)));
     volume = 2*pi*double(int(integrant, lowerBound, upperBound));
 end
+
+% Included filler cylinder between lower/upper bound and axis of rotation.
+% Conditions b/w the lower bound being greater than axis value, upper
+% bound being lesser than axis value, or if a bound matches the axis value.
+if (lowerBound > axisValue)
+    inner_radius = abs(lowerBound - axisValue);
+    
+    if (lower(axisOri) == 'y') % Horizontal inner cylinder
+        inner_cyl_height = double(g(upperBound) - g(lowerBound));
+    else % Vertical Line
+        inner_cyl_height = double(f(upperBound) - f(lowerBound));
+    end
+    inner_cyl_vol = pi*(inner_radius^2)*inner_cyl_height;
+    
+elseif (upperBound < axisValue)
+   inner_radius = abs(axisValue - upperBound);
+   
+    if (lower(axisOri) == 'y') % Horizontal inner cylinder, or vertical.
+        inner_cyl_height = double(upperBound - g(upperBound));
+    else %
+        inner_cyl_height = double(upperBound - f(upperBound));
+    end
+    inner_cyl_vol = pi*(inner_radius^2)*inner_cyl_height;
+    
+else
+    inner_cyl_vol = 0;
+end
+
+volume = volume + inner_cyl_vol;
 end
 
