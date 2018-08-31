c42c390a1f84a7f513f43d9fd71cb9d58deeb8ab
Andrew <siggles1418@gmail.com>
2018-08-31 18:33:00 -0400

    Disk and shell volume actual calculation methods almost complete
    Now must be rigth for cases when f(x) = 2^x and bounds are negative.

--- VolumesWithIntegrals/shellmethod1.m
+++ VolumesWithIntegrals/shellmethod1.m
@@ -12,7 +12,7 @@ function volume = shellmethod1(funcString, lowerBound, upperBound, axisOri, axis
 %       1. Convert function string to symbolic expression.
 %       2. Integrate the function squared times the variable the integral
 %          is done in respect to. Multiply the integral by 2*pi.
-%       3. 
+%       3. s
 
 syms x
 f(x) = str2sym(funcString);
@@ -23,22 +23,60 @@ radius = x - axisValue; % Radius for each shell, in the circumference formula to
 % If rotating around vertical line, 
 if (lower(axisOri) == 'x')
      integrant = abs(radius*f(x)); % |(x-radius)*f(x)|
-else % If rotation done paralell to a horizontal line, integrate function's inverse.
-    integrant = abs(radius*(g(upperBound) - g(x)));
+     
+% If rotation done paralell to a horizontal line, integrate function's inverse.
+elseif  (lower(axisOri) == 'y')
     
-   
+    % Area is bounded in area which is in negative-negative quadrant.
+    if (g(lowerBound) <= 0 && g(upperBound) <= 0)
+        if (funcString == "x")
+            integrant = abs(radius*(g(lowerBound) - g(x)));
+        else
+            integrant = abs(radius*(g(upperBound) - g(x)));
+        end
+    % Otherwise, bound one end of each shell's length by constant,
+    % g(upperBound)
+    else
+        integrant = abs(radius*(g(upperBound) - g(x)));
+    end
 end
 
 volume = 2*pi*double(int(integrant, lowerBound, upperBound));
 
- if (lower(axisOri) == 'y' && g(lowerBound) >= axisValue)
-     % If there is gap b/w inverse lower boudn and axis, add that inner
-     % volume to the total volume.
-     if (axisValue >= 0)
-        fillerShellVolume = double(int(abs(g(lowerBound) - g(upperBound)), axisValue, g(lowerBound)));
-     else
-        fillerShellVolume = double(int(abs(g(lowerBound) - g(upperBound)), 0, g(lowerBound)));
-     end
-     volume = volume + 2*pi*fillerShellVolume;
- end
+% If there is gap b/w inverse lower boudn and axis, add that inner
+% volume to the total volume.
+if (lower(axisOri) == 'y') 
+    innerShellLength = g(lowerBound) - g(upperBound);
+    
+    % Horizontal axis lower than lowerbound on y-axis, but greater than
+    % y=0, rotate area b/w lowerbound and axis value, in which diff b/w
+    % both is width and diff b/w counterparts of lower and upper bounds
+    % along x-axis is the length (shell height) of the shell.
+  if (lowerBound >= axisValue)
+    if (lowerBound >= 0 && upperBound >= 0)
+      if (axisValue >= 0)
+        fillerShellVolume = double(int(abs(radius*innerShellLength), axisValue, lowerBound));
+      else
+        fillerShellVolume = double(int(abs(radius*innerShellLength), 0, lowerBound));
+      end
+      
+    elseif (lowerBound <= 0 && upperBound <= 0)
+        fillerShellVolume = double(int(abs(radius*innerShellLength), upperBound, 0));
+    end
+  elseif (upperBound <= axisValue)
+    % Volume is negative
+    if (upperBound <= 0)
+      if (axisValue <= 0)
+        fillerShellVolume = double(int(abs(radius*innerShellLength), upperBound, axisValue));
+      else
+        fillerShellVolume = double(int(abs(radius*innerShellLength), upperBound, 0));
+      end
+%     end
+    % The area is positive, so rotate it
+    elseif (upperBound > 0)
+        fillerShellVolume = double(int(abs(radius*innerShellLength), 0, lowerBound));
+    end
+  end
+  volume = volume + 2*pi*fillerShellVolume;
+end
 end
